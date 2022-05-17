1ef0a9bba655b866541b740c5f77b2e5e80397a4
villerca <villerca@gmail.com>
2022-05-17 18:31:28 -0400

    corrigiendo errores de escritura

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import './style/global.scss';
+import './styles/global.scss';
 
 ReactDOM.render(<App />, document.getElementById('app'));
